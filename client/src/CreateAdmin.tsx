c2ff2ff4382ad9d7ae8c01ae5258bef2eea19e28
Max Eddy <maxiscool@gmail.com>
2021-03-05 20:47:25 -0800

    Clean up.

--- client/src/CreateAdmin.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import AuthForm from "./AuthForm";
-
-export default function CreateAdmin() {
-  return <AuthForm title="Setup" submitLabel="Create Admin" />;
-}
--- client/src/SignIn.tsx
+++ client/src/SignIn.tsx
@@ -1,6 +1,80 @@
 import React from "react";
-import AuthForm from "./AuthForm";
+import {
+  Avatar,
+  Button,
+  Container,
+  TextField,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+import { LockOutlined } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 export default function SignIn() {
-  return <AuthForm title="Sign In" />;
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlined />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign In
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="on"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
 }
