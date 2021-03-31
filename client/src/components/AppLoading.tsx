ce5a668d1847491577f812735d438b259293e39e
Max Eddy <maxiscool@gmail.com>
2021-03-30 22:35:23 -0700

    Loading spinner

--- client/src/components/AppLoading.tsx
+++ client/src/components/AppLoading.tsx
@@ -1,10 +1,27 @@
-import { Container } from "@material-ui/core";
+import { Container, makeStyles } from "@material-ui/core";
 import LoopIcon from "@material-ui/icons/Loop";
 
+const useStyles = makeStyles((theme) => ({
+  "@keyframes spin": {
+    "0%": { transform: "rotate(0deg)" },
+    "100%": { transform: "rotate(359deg)" },
+  },
+  container: {
+    textAlign: "center",
+  },
+  loader: {
+    marginTop: theme.spacing(2),
+    fontSize: "5em",
+    animation: "$spin 2s linear infinite",
+  },
+}));
+
 export default function AppLoading() {
+  const classes = useStyles();
+
   return (
-    <Container>
-      <LoopIcon />
+    <Container className={classes.container}>
+      <LoopIcon className={classes.loader} />
     </Container>
   );
 }
